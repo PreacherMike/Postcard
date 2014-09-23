f1e46cb8c4d57e5bdc97350cce38871164f765bf
Michael Morton <michaelmorton@Michaels-MacBook-Pro.local>
2014-09-22 21:59:37 -0400

    added a comment

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // Code will evaluate when we press the button
-        // Adding a comment here
+        // Adding a comment here MORE
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         enterMessageTextField.text = ""
